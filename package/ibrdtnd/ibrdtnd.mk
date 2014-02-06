3d0e43cb35d38c2679174ce0713e5890ac0ef8dc
Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
2014-02-06 16:13:28 +0100

    ibrdtnd: new package
    This package contains the daemon (dtnd) of IBR-DTN, a modular and lightweight
    implementation of the bundle protocol (RFC 5050). https://github.com/ibrdtn/ibrdtn

---
+++ package/ibrdtnd/ibrdtnd.mk
@@ -0,0 +1,26 @@
+#############################################################
+#
+# ibrdtnd
+#
+#############################################################
+IBRDTND_VERSION:=0.10.2
+IBRDTND_SOURCE:=ibrdtnd-$(IBRDTND_VERSION).tar.gz
+IBRDTND_SITE:=http://www.ibr.cs.tu-bs.de/projects/ibr-dtn/releases
+IBRDTND_LIBTOOL_PATCH:=NO
+IBRDTND_DEPENDENCIES:=dtndht ibrcommon ibrdtn libdaemon openssl zlib sqlite
+IBRDTND_INSTALL_STAGING:=YES
+IBRDTND_INSTALL_TARGET:=YES
+
+ifeq ($(BR2_PREFER_STATIC_LIB),y)
+IBRDTND_MAKE_OPT += LDFLAGS+="-all-static"
+endif
+
+IBRDTND_CONF_OPT =	\
+		--with-sqlite \
+		--with-dtnsec \
+		--with-compression \
+		--with-tls
+		
+
+$(eval $(autotools-package))
+
